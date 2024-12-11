3695c8a09486f3fffbab0f389ab83b5c9f6c573f
safebuster2 <safebuster2@gmail.com>
2024-12-11 18:55:01 +0300

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    part1().map(|sum| println!("{}", sum))
+    part1().map(|sum| println!("{}", sum))?;
+    part2().map(|sum| println!("{}", sum))
 }
 
 fn part1() -> io::Result<i32> {
@@ -17,12 +18,38 @@ fn part1() -> io::Result<i32> {
     Ok(sum)
 }
 
+fn part2() -> io::Result<i32> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let text = reader
+        .lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>()
+        .join("");
+
+    let sum = parse_line_with_do_and_dont(text.as_str());
+
+    Ok(sum)
+}
+
 fn parse_line(line: &str) -> i32 {
     line.split("mul(")
         .map(|str| str.split(")").next().unwrap())
         .map(|str| str.split(","))
         .filter_map(|v| parse_mul(v.collect()))
         .sum()
+}
+
+fn parse_line_with_do_and_dont(line: &str) -> i32 {
+    let parts: Vec<&str> = line.split("don't()").collect();
+    let mut text = parts[0].to_owned();
+    for i in 1..parts.len() {
+        let mut tmp = parts[i].split("do()").collect::<Vec<&str>>();
+        tmp.remove(0);
+        text += tmp.join("").as_str();
+    }
+    parse_line(text.as_str())
 }
 
 fn parse_mul(parts: Vec<&str>) -> Option<i32> {
@@ -45,5 +72,13 @@ mod tests {
         let result =
             parse_line("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
         assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn parse_line_with_do_and_dont_test() {
+        let result = parse_line_with_do_and_dont(
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+        );
+        assert_eq!(result, 48);
     }
 }
