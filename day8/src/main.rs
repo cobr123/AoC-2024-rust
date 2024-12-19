983c4557862f082887d14dfa82d3abbfb0db10c9
safebuster2 <safebuster2@gmail.com>
2024-12-19 17:40:13 +0300

    day8 part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,7 +4,8 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    part1().map(|count| println!("{}", count))
+    part1().map(|count| println!("{}", count))?;
+    part2().map(|count| println!("{}", count))
 }
 
 fn part1() -> io::Result<usize> {
@@ -17,6 +18,20 @@ fn part1() -> io::Result<usize> {
         .collect::<Vec<String>>()
         .join("\n");
     let count = count_antinodes(text.trim());
+
+    Ok(count)
+}
+
+fn part2() -> io::Result<usize> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let text = reader
+        .lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>()
+        .join("\n");
+    let count = count_antinodes_with_harmonics(text.trim());
 
     Ok(count)
 }
@@ -84,6 +99,137 @@ fn get_antinode(width: i32, height: i32, p0: &Pos, p1: &Pos) -> Vec<Pos> {
     res
 }
 
+fn count_antinodes_with_harmonics(text: &str) -> usize {
+    let (width, height, map) = parse_text(text);
+    let mut antinodes = HashSet::new();
+    for list in map {
+        for pair in list.iter().combinations(2) {
+            let p0 = pair.get(0).unwrap();
+            let p1 = pair.get(1).unwrap();
+            get_antinode_from_left(&mut antinodes, width, height, p0, p1);
+            get_antinode_from_right(&mut antinodes, width, height, p0, p1);
+        }
+    }
+    // print_antinodes(width, height, &antinodes);
+    antinodes.len()
+}
+
+fn print_antinodes(width: i32, height: i32, antinodes: &HashSet<Pos>) {
+    for r in 0..height {
+        for c in 0..width {
+            if antinodes.iter().contains(&Pos { row: r, col: c }) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
+    // antinodes.iter().for_each(|x| println!("{:?}", x));
+}
+
+fn get_antinode_from_left(
+    antinodes: &mut HashSet<Pos>,
+    width: i32,
+    height: i32,
+    p0: &Pos,
+    p1: &Pos,
+) {
+    antinodes.insert(*p0);
+    antinodes.insert(*p1);
+    // print_antinodes(width, height, &antinodes);
+    if p0.col == p1.col {
+        if p0.row < p1.row {
+            let diff_row = p1.row - p0.row;
+            let (row, col) = (p0.row - diff_row, p0.col);
+            if col >= 0 && col < width && row >= 0 && row < height {
+                let pp = Pos { row, col };
+                get_antinode_from_left(antinodes, width, height, &pp, p1);
+            }
+        } else if p1.row < p0.row {
+            let diff_row = p0.row - p1.row;
+            let (row, col) = (p1.row - diff_row, p0.col);
+            if col >= 0 && col < width && row >= 0 && row < height {
+                let pp = Pos { row, col };
+                get_antinode_from_left(antinodes, width, height, &pp, p1);
+            }
+        }
+    } else if p0.col < p1.col {
+        let diff_col = p1.col - p0.col;
+        let (row, col) = if p0.row < p1.row {
+            let diff_row = p1.row - p0.row;
+            (p0.row - diff_row, p0.col - diff_col)
+        } else {
+            let diff_row = p0.row - p1.row;
+            (p0.row + diff_row, p0.col - diff_col)
+        };
+        if col >= 0 && col < width && row >= 0 && row < height {
+            let pp1 = Pos { row, col };
+            // println!("l {:?} {:?}, {:?} {:?}", pp1, pp0, p0, p1);
+            get_antinode_from_left(antinodes, width, height, &pp1, p0);
+        } else {
+            // println!(
+            //     "else l {:?} height={height}, width={width}",
+            //     Pos { row, col }
+            // )
+        }
+    } else {
+        // println!("swap l {:?} {:?}", p0, p1);
+        get_antinode_from_left(antinodes, width, height, p1, p0);
+    }
+}
+
+fn get_antinode_from_right(
+    antinodes: &mut HashSet<Pos>,
+    width: i32,
+    height: i32,
+    p0: &Pos,
+    p1: &Pos,
+) {
+    antinodes.insert(*p0);
+    antinodes.insert(*p1);
+    // print_antinodes(width, height, &antinodes);
+    if p0.col == p1.col {
+        if p1.row > p0.row {
+            let diff_row = p1.row - p0.row;
+            let (row, col) = (p1.row + diff_row, p0.col);
+            if col >= 0 && col < width && row >= 0 && row < height {
+                let pp = Pos { row, col };
+                get_antinode_from_right(antinodes, width, height, &pp, p0);
+            }
+        } else if p0.row > p1.row {
+            let diff_row = p0.row - p1.row;
+            let (row, col) = (p0.row + diff_row, p0.col);
+            if col >= 0 && col < width && row >= 0 && row < height {
+                let pp = Pos { row, col };
+                get_antinode_from_right(antinodes, width, height, &pp, p0);
+            }
+        }
+    } else if p0.col > p1.col {
+        let diff_col = p0.col - p1.col;
+        let (row, col) = if p0.row < p1.row {
+            let diff_row = p1.row - p0.row;
+            (p0.row - diff_row, p0.col + diff_col)
+        } else {
+            let diff_row = p0.row - p1.row;
+            (p0.row + diff_row, p0.col + diff_col)
+        };
+        if col >= 0 && col < width && row >= 0 && row < height {
+            let pp1 = Pos { row, col };
+            // println!("r {:?} {:?}, {:?} {:?}", pp1, pp0, p0, p1);
+            get_antinode_from_right(antinodes, width, height, p0, &pp1);
+        } else {
+            // println!(
+            //     "else r {:?} height={height}, width={width}",
+            //     Pos { row, col }
+            // )
+        }
+    } else {
+        // println!("swap r {:?} {:?}", p0, p1);
+        get_antinode_from_right(antinodes, width, height, p1, p0);
+    }
+}
+
 #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 struct Pos {
     row: i32,
@@ -116,7 +262,7 @@ fn parse_text(text: &str) -> (i32, i32, Vec<Vec<Pos>>) {
         .filter(|(_char, list)| list.len() > 1)
         .map(|(_char, list)| list)
         .collect();
-    (row, col, positions)
+    (col, row, positions)
 }
 
 #[cfg(test)]
@@ -174,5 +320,156 @@ mod tests {
 ..........",
         );
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics9_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........",
+        );
+        assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics34_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............",
+        );
+        assert_eq!(result, 34);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_vt_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "......
+......
+.....0
+......
+.....0",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_vb_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "......
+.....0
+......
+.....0
+......
+......",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_btl_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "......
+....0.
+.....0",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_btl2_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "............
+............
+............
+............
+............
+............
+............
+............
+........A...
+.........A..
+............
+............",
+        );
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_btr_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "......
+....0.
+...0..",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_tbl_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            ".....0
+....0.
+......",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_tbl2_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "............
+........0...
+............
+.......0....
+............
+............
+............
+............
+............
+............
+............
+............",
+        );
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_tbr_test() {
+        let result = crate::count_antinodes_with_harmonics(
+            "...0..
+....0.
+......",
+        );
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_hr_test() {
+        let result = crate::count_antinodes_with_harmonics("0.0..");
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn count_antinodes_with_harmonics_hl_test() {
+        let result = crate::count_antinodes_with_harmonics("...0.0");
+        assert_eq!(result, 3);
     }
 }
